be6b0c18efe489b600548dad534cf5dfc0f24e09
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-22 11:39:00 +0800

    completed guided project

---
+++ data/migrations/20200522112040_fruits-color.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table("fruits", tbl => {
+    tbl.string("color", 128);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("fruits", tbl => {
+    tbl.dropColumn("color");
+  });
+};
--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,12 @@ module.exports = {
     connection: {
       filename: "./data/produce.db3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
